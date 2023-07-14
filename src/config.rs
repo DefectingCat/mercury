dc3f494bc7fcee3ebfc82a896b8572fbbfbfce5e
DefectingCat <i@defect.ink>
2023-07-14 16:56:53 +0800

    feat: add log level to config

--- src/config.rs
+++ src/config.rs
@@ -4,18 +4,50 @@ use std::{
     path::PathBuf,
 };
 
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 
 use crate::error::MResult;
 
 fn default_port() -> usize {
     4000
 }
+fn default_log_level() -> LogLevel {
+    LogLevel::INFO
+}
+
+#[derive(Debug, Clone)]
+pub enum LogLevel {
+    INFO,
+}
+impl Serialize for LogLevel {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(match self {
+            LogLevel::INFO => "info",
+        })
+    }
+}
+impl<'de> Deserialize<'de> for LogLevel {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Ok(match s.as_str() {
+            "info" => LogLevel::INFO,
+            _ => LogLevel::INFO,
+        })
+    }
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Config {
     #[serde(default = "default_port")]
     pub port: usize,
+    #[serde(default = "default_log_level")]
+    pub log_level: LogLevel,
 }
 
 impl Config {
--- src/logger.rs
+++ src/logger.rs
@@ -9,7 +9,8 @@ pub fn logger_init() -> MResult<()> {
     let formattin_layer = fmt::layer()
         .pretty()
         .with_thread_ids(true)
-        .with_target(false)
+        // .with_target(false)
+        .with_file(false)
         .with_writer(std::io::stdout);
 
     let filter = filter::LevelFilter::DEBUG;
